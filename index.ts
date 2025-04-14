b361db0b6aeb02decbcba27027a8d91372fe5e74
blesswrld <roninfortnite71@gmail.com>
2025-04-14 19:10:38 +0300

    task currRate work with typization

--- index.ts
+++ index.ts
@@ -1,50 +1,24 @@
-// const isBirthdayData: boolean = true; // Фиксируем тип данных boolean
-// let ageData: number = 40;
-// const userNameData: string = "John"; // Фиксируем тип данных string
+const currRate = "1.05";
 
-let salary: number; // Фиксируем тип данных number
-salary = 5000; // Задаем значение для глобальной переменной
-
-// Статичные данные из JSON (для примеры)
-const userData =
-    '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
-
-// Парсим userData
-const userObj: {
-    isBirthdayData: boolean;
-    ageData: number;
-    userNameData: string;
-} = JSON.parse(userData);
-console.log(userObj.smt()); // any
-
-// Стандартная функция
-// Передаем аргументы в функцию указывая тип данных каждой переменной
-// function logBrtMsg(isBirthday: boolean, age: number, userName: string): string {
-//     // указываем тип выводимых данных : string, но с обязательным условием (НАЛИЧИЕ else)
-//     if (isBirthday) {
-//         // выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
-//         return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
-//     } else {
-//         return "Error";
-//     }
-// }
-
-// Стрелочная функция
-const logBrtMsg = (
-    isBirthday: boolean,
-    age: number,
-    userName: string
-): string => {
-    // указываем тип выводимых данных : string, но с обязательным условием (НАЛИЧИЕ else)
-    if (isBirthday) {
-        // выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
-        return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
-    } else {
-        return "Error";
-    }
+const fetchCurr = (response: string) => {
+    const data = JSON.parse(response);
+    return data;
 };
 
-logBrtMsg(isBirthdayData, 40, userNameData);
+function transferEurToUsd(
+    available: boolean,
+    amount: number,
+    commission: number
+) {
+    if (available) {
+        let res = fetchCurr(currRate) * amount * commission;
+        console.log(res);
+    } else {
+        console.log("Сейчас обмен недоступен");
+    }
+}
+
+transferEurToUsd(true, 500, 1.05);
 
 // tsc index.ts (команда в терминале для запуска компилятора ts кода)
 // tsc -help (команда в терминале для помощи с настройками)
