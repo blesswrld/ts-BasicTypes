c8e31974733026fceb7ffe01278b3ae53ac968c1
blesswrld <roninfortnite71@gmail.com>
2025-04-14 17:12:23 +0300

    system of types, system of types in functions

--- index.ts
+++ index.ts
@@ -1,24 +1,36 @@
-// Фиксируем тип данных string
-/* let userName: string = "Tamerlan";
-// userName.isInteger();
+const isBirthdayData: boolean = true; // Фиксируем тип данных boolean
+let ageData: number = 40;
+const userNameData: string = "John"; // Фиксируем тип данных string
 
-userName = "Ivan"; */ // Переиспользуем переменную
+// Стандартная функция
+// Передаем аргументы в функцию указывая тип данных каждой переменной
+// function logBrtMsg(isBirthday: boolean, age: number, userName: string): string {
+//     // указываем тип выводимых данных : string, но с обязательным условием (НАЛИЧИЕ else)
+//     if (isBirthday) {
+//         // выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
+//         return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+//     } else {
+//         return "Error";
+//     }
+// }
 
-// '', "", `` string type
-// 10, 0.5, 0.00001, -50, 4e10 number type
-// true, false boolean type
+// Стрелочная функция
+const logBrtMsg = (
+    isBirthday: boolean,
+    age: number,
+    userName: string
+): string => {
+    // указываем тип выводимых данных : string, но с обязательным условием (НАЛИЧИЕ else)
+    if (isBirthday) {
+        // выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
+        return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+    } else {
+        return "Error";
+    }
+};
+
+logBrtMsg(isBirthdayData, 40, userNameData);
 
 // tsc index.ts (команда в терминале для запуска компилятора ts кода)
 // tsc -help (команда в терминале для помощи с настройками)
 // ts-node index.ts (команда для запуска ноды + ts файл -- удобно для быстрого вывода в консоль)
-
-const isBirthday: boolean = true; // Фиксируем тип данных boolean
-let age: number; // Фиксируем тип данных number
-const userName: string = "John";
-
-age = 40; // Добавляем значение для глобальной переменной
-
-if (isBirthday) {
-    // выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
-    console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
-}
