6e017e0b8079962d56aebaeb38e14b6baba22a42
blesswrld <roninfortnite71@gmail.com>
2025-04-14 16:47:30 +0300

    basic types of data, string methods and so on

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var str = "str";
-console.log(str);
+let userName = "Tamerlan";
+userName.isInteger();
 // tsc index.ts (команда в терминале для запуска компилятора typescript кода)
 // tsc -help (команда в терминале для помощи с настройками)
--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
-const str = "str";
+// Фиксируем тип данных string
+/* let userName: string = "Tamerlan";
+// userName.isInteger();
 
-console.log(str);
+userName = "Ivan"; */ // Переиспользуем переменную
+
+// '', "", `` string type
+// 10, 0.5, 0.00001, -50, 4e10 number type
+// true, false boolean type
 
 // tsc index.ts (команда в терминале для запуска компилятора ts кода)
 // tsc -help (команда в терминале для помощи с настройками)
 // ts-node index.ts (команда для запуска ноды + ts файл -- удобно для быстрого вывода в консоль)
+
+const isBirthday: boolean = true; // Фиксируем тип данных boolean
+let age: number; // Фиксируем тип данных number
+const userName: string = "John";
+
+age = 40; // Добавляем значение для глобальной переменной
+
+if (isBirthday) {
+    // выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
+    console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+}
