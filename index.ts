476854190bd1f7fd9244baac233251c6608e7a11
blesswrld <roninfortnite71@gmail.com>
2025-04-16 07:01:31 +0300

    task calculatePayments work with typization, destructuring obj, arr and so on

--- index.ts
+++ index.ts
@@ -1,71 +1,53 @@
-const isBirthdayData: boolean = true; // Фиксируем тип данных boolean
-let ageData: number = 40; // Фиксируем тип данных number
-const userNameData: string = "John"; // Фиксируем тип данных string
-
-// Объект с 3 свойствами
-const userData = {
-    isBirthdayData: true, // boolean
-    ageData: 40, // number
-    userNameData: "John", // string
-    messages: {
-        error: "Error", // Значение свойства error
-    }, // Вложенный объект для теста деструктуризации
+const electricityUserData = {
+    readings: 95,
+    units: "kWt",
+    mode: "double",
 };
 
-// Стрелочная функция
-const createError = (msg: string) => {
-    throw new Error(msg); // выбрасываем ошибку с типом never (который ничего не возвращает и ничего не содержит)
+const waterUserData = {
+    readings: 3,
+    units: "m3",
 };
 
-function logBrtMsg({
-    // Выполняем деструктуризацию
-    isBirthdayData,
-    ageData,
-    userNameData,
-    messages: { error },
-}: {
-    // Вытаскиваем свойства из объекта userData
-    isBirthdayData: boolean;
-    ageData: number;
-    userNameData: string;
-    messages: { error: string }; // Вытаскиваем вложенный объект messages и фиксируем его тип
-}): string {
-    // Указываем тип выводимых данных : string, но с обязательным условием (НАЛИЧИЕ else)
-    if (isBirthdayData) {
-        // Выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
-        return `Congrats ${userNameData.toUpperCase()}, age: ${ageData + 1}`;
+const elRate = 0.45;
+const wRate = 2;
+
+const monthPayments: number[] = [0, 0]; // [electricity, water]
+
+const calculatePayments = (
+    { readings, mode }: { readings: number; mode: string },
+    wData: { readings: number },
+    elRate: number,
+    wRate: number
+) => {
+    if (mode === "double" && readings < 50) {
+        monthPayments[0] = readings * elRate * 0.7;
     } else {
-        return createError(error); // Вызываем функцию которая выбрасывает ошибку из вложенного объекта для прекращения условий
+        monthPayments[0] = readings * elRate;
     }
-}
 
-console.log(logBrtMsg(userData)); // Выводим в консоль результат
+    monthPayments[1] = wData.readings * wRate;
+};
 
-// Фиксируем тип данных string на массиве (чтобы не передавались другие типы данных)
-const departments: string[] = ["dev", "design", "marketing"]; // Массив с данными об отделах разработки
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
 
-const department = departments[0]; // массив string
+const sendInvoice = (
+    [el, water]: number[],
+    electricityUserData: { readings: number; units: string },
+    waterUserData: { readings: number; units: string }
+): string => {
+    const text = `    Hello!
+    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+    It will cost: ${el}€
+    
+    This month you used ${waterUserData.readings} ${waterUserData.units} of water
+    It will cost: ${water}€`;
 
-// departments.push(5);
-const report = departments
-    .filter((d: string) => d !== "dev") // Фильтруем отделы по 'dev'
-    // Если условие ! не выполнилось вызываем метод map() и добавляем для 'dev' отдела строку - done
-    .map(
-        (d: string) => `
-    ${d} - done
-`
-    );
+    return text;
+};
 
-// Фиксируем тип данных number на под-массиве (чтобы не передавались другие типы данных)
-const nums: number[][] = [
-    // Массив для комплексных данных
-    [3, 2, 1],
-    [1, 2, 3],
-    // ...
-];
-
-const [firstElem] = report; // Вытаскиваем первый элемент из переменной report
-console.log(firstElem); // Выводим в консоль ('dev' отпадает из-за фильтрации)
+const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
+console.log(invoice);
 
 // const smth: never = null;
 
