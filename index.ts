afcaa8dfd8230e99822b39fcb213c6c15f3afd9b
blesswrld <roninfortnite71@gmail.com>
2025-04-16 04:32:32 +0300

    type never

--- index.ts
+++ index.ts
@@ -1,24 +1,27 @@
-const currRate = "1.05";
+const isBirthdayData: boolean = true; // Фиксируем тип данных boolean
+let ageData: number = 40;
+const userNameData: string = "John"; // Фиксируем тип данных string
 
-const fetchCurr = (response: string) => {
-    const data = JSON.parse(response);
-    return data;
+// Стрелочная функция
+const createError = (msg: string) => {
+    throw new Error(msg); // выбрасываем ошибку с типом never (который ничего не возвращает и ничего не содержит)
 };
 
-function transferEurToUsd(
-    available: boolean,
-    amount: number,
-    commission: number
-) {
-    if (available) {
-        let res = fetchCurr(currRate) * amount * commission;
-        console.log(res);
-    } else {
-        console.log("Сейчас обмен недоступен");
+// Стрелочная функция
+function logBrtMsg(isBirthday: boolean, age: number, userName: string): string {
+    // указываем тип выводимых данных : string, но с обязательным условием (НАЛИЧИЕ else)
+    if (isBirthday === true) {
+        // выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
+        return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+    } else if (isBirthday === false) {
+        return "Too bad";
     }
+    return createError("Error"); // Вызываем функцию которая выбрасывает ошибку для прекращения условий
 }
 
-transferEurToUsd(true, 500, 1.05);
+logBrtMsg(isBirthdayData, 40, userNameData);
+
+// const smth: never = null;
 
 // tsc index.ts (команда в терминале для запуска компилятора ts кода)
 // tsc -help (команда в терминале для помощи с настройками)
