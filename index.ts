e87efe47eff89a288025a98413e7b3d4ea322a03
blesswrld <roninfortnite71@gmail.com>
2025-04-17 16:57:23 +0300

    Union-Types

--- index.ts
+++ index.ts
@@ -1,61 +1,15 @@
-// Объект с 3 свойствами
-const userData = {
-    isBirthdayData: true, // boolean
-    ageData: 40, // number
-    userNameData: "John", // string
-    messages: {
-        error: "Error", // Значение свойства error
-    }, // Вложенный объект для теста деструктуризации
-};
+const message: string | number = 5; // | - Оператор ИЛИ в TS
+const messages: string[] | number[] = ["a", "b"]; // Может быть либо массивом строк, либо массивом чисел
 
-// Tuples (Кортежи)
-// Описываем структуру данных и фиксируем их тип (по-порядку)
-const userDataTuple: [...boolean[], number, string] = [
-    // Разворачиваем массив булиновых типов используя Spread оператор (для доп.значений)
-    true,
-    false,
-    40,
-    "John",
-];
-// userDataTuple[3];
-// userDataTuple.push(50);
-// userDataTuple[3];
-
-const res = userDataTuple.map((t) => `${t} - data`);
-
-// Назначаем свойства для значений кортежа по-порядку
-const [bthd, age, userName] = userDataTuple;
-
-// Стрелочная функция
-const createError = (msg: string) => {
-    throw new Error(msg); // выбрасываем ошибку с типом never (который ничего не возвращает и ничего не содержит)
-};
-
-function logBrtMsg({
-    // Выполняем деструктуризацию
-    isBirthdayData,
-    ageData,
-    userNameData,
-    messages: { error },
-}: {
-    // Вытаскиваем свойства из объекта userData
-    isBirthdayData: boolean;
-    ageData: number;
-    userNameData: string;
-    messages: { error: string }; // Вытаскиваем вложенный объект messages и фиксируем его тип
-}): string {
-    // Указываем тип выводимых данных : string, но с обязательным условием (НАЛИЧИЕ else)
-    if (isBirthdayData) {
-        // Выводим в консоль и приводом к верхнему регистру при помощи метода toUpperCase, (засчет фиксированного типа данных на переменной userName)
-        return `Congrats ${userNameData.toUpperCase()}, age: ${ageData + 1}`;
-    } else {
-        return createError(error); // Вызываем функцию которая выбрасывает ошибку из вложенного объекта для прекращения условий
-    }
+// Функция принимает msg, который может быть строкой, числом или булевым значением
+// Union-Типы
+function printMsg(msg: string | number | boolean): void {
+    console.log(msg);
 }
 
-console.log(logBrtMsg(userData)); // Выводим в консоль результат
-
-// const smth: never = null;
+// Вызов функции
+printMsg(4);
+printMsg("Hello World!");
 
 // tsc index.ts (команда в терминале для запуска компилятора ts кода)
 // tsc -help (команда в терминале для помощи с настройками)
